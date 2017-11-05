d3fa78bf62c30a9f0228f747370d65d0a8b5eef5
longfeizheng <longfei_zheng@163.com>
2017-11-05 22:08:38 +0800

    LoggerTest类中新增@Sl4j的注解

--- src/test/java/cn/merryyou/logback/LoggerTest.java
+++ src/test/java/cn/merryyou/logback/LoggerTest.java
@@ -3,8 +3,6 @@ package cn.merryyou.logback;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,14 +17,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @Slf4j
 public class LoggerTest {
-//    private final Logger logger = LoggerFactory.getLogger(LoggerTest2.class);
-    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+    //    private final Logger logger = LoggerFactory.getLogger(LoggerTest2.class);
+//    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
 
     @Test
-    public void test1(){
-        logger.debug("debug...");
-        logger.info("info...");
-        logger.info("name: {} ,age: {}","tom",20);
-        logger.error("error");
+    public void test1() {
+//        logger.debug("debug...");
+//        logger.info("info...");
+//        logger.info("name: {} ,age: {}","tom",20);
+//        logger.error("error");
+
+        log.debug("debug...");
+        log.info("info...");
+        log.info("name: {} ,age: {}", "tom", 20);
+        log.error("error");
     }
 }
