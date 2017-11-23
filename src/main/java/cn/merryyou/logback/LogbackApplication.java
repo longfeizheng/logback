cb051d27830fd2cda9807fd636519f1b1a6a0003
longfeizheng <longfei_zheng@163.com>
2017-11-23 20:01:29 +0800

    添加定时任务支持

--- src/main/java/cn/merryyou/logback/LogbackApplication.java
+++ src/main/java/cn/merryyou/logback/LogbackApplication.java
@@ -2,8 +2,10 @@ package cn.merryyou.logback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LogbackApplication {
 
 	public static void main(String[] args) {
